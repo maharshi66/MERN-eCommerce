35246b573dd37686ec99071173063068a26e33fe
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-03-12 12:56:10 +0530

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 style={{textAlign: 'center'}}> Hello from Proshop </h1>
+    </div>
+  );
+}
+
+export default App;
