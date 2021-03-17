ce13b10a5a0a1b46a2ece8f82c2e34809ebfcd1c
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-03-17 20:12:20 +0530

    Added frontend code for sign in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
+import {useDispatch, useSelector} from 'react-redux'
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+    const userLogin = useSelector(state => state.userLogin)
+    
+    const {userInfo} = userLogin 
+    
+    const dispatch = useDispatch()
+    
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+    
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -19,13 +31,24 @@ const Header = () => {
                             <i className="fas fa-shopping-cart"></i> Cart
                         </Nav.Link>
                         </LinkContainer>
-                        
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>
+                                        Profile
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>
+                                    Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        ) 
+                        :
                         <LinkContainer to='/login'>
-                        <Nav.Link>
-                            <i className="fas fa-user"></i> Sign In
-                        </Nav.Link>
-                        </LinkContainer>
-                        
+                            <Nav.Link>
+                                <i className="fas fa-user"></i> Sign In
+                            </Nav.Link>
+                        </LinkContainer>}                        
                         </Nav>
                     </Navbar.Collapse>
                     
